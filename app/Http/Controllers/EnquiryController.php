7cbbaeedea01fecb572b178b9f5ff531b1fd8439
orbitlink10 <orbitlinksolutions@gmail.com>
2026-06-06 11:55:40 +0300

    Your commit message

--- app/Http/Controllers/EnquiryController.php
+++ app/Http/Controllers/EnquiryController.php
@@ -15,7 +15,7 @@ class EnquiryController extends Controller
                 'name' => 'Caracal',
                 'subtitle' => 'Expeditions',
                 'full_name' => 'Caracal Expeditions',
-                'logo_url' => asset('images/caracal-expeditions-profile.svg'),
+                'logo_url' => asset('images/caracal-expeditions-profile.jpg'),
             ],
             'companyEmail' => 'info@caracalexpeditions.co.ke',
             'companyPhone' => '+254701942724',
